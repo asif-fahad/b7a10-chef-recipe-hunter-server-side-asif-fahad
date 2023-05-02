25443ea9a50c4791bb27e80cc6ee2352602cf5be
asif-fahad <asif123fahad@gmail.com>
2023-05-02 14:14:05 +0600

    data added all

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json')
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Chefs of Italy');
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChefs = chefs.find(c => c.id === id);
+    res.send(selectedChefs)
 });
 
 app.listen(port, () => {
